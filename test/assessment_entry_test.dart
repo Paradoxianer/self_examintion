f8b38867e6b6aeb28e5bf07a80770034082c2eec
paradoxon <blablabla!1865Atlassian>
2023-10-31 22:09:09 +0000

    Added first Assesmententrytest... but no implementation.. jet

---
+++ test/assessment_entry_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:self_examintion/models/assessment_entry.dart';
+
+
+/*void main() {
+  group('AssessmentEntry', () {
+    test('toJson method converts object to JSON', () {
+      // Create an AssessmentEntry object
+      final entry = AssessmentEntry(
+        id: 1,
+        author: 'John Doe',
+        assessmentData: {1: 3, 2: 2, 3: 4},
+      );
+
+      // Convert the object to JSON
+      final json = entry.toJson();
+
+      // Define the expected JSON representation
+      final expectedJson = {
+        'id': 1,
+        'author': 'John Doe',
+        'assessmentData': {'1': 3, '2': 2, '3': 4},
+      };
+
+      // Compare the actual JSON with the expected JSON
+      expect(json, expectedJson);
+    });
+
+    test('fromJson method converts JSON to object', () {
+      // Define a JSON representation
+      final json = {
+        'id': 1,
+        'author': 'John Doe',
+        'assessmentData': {'1': 3, '2': 2, '3': 4},
+      };
+
+      // Convert the JSON to an AssessmentEntry object
+      final entry = AssessmentEntry.fromJson(json);
+
+      // Define the expected AssessmentEntry object
+      final expectedEntry = AssessmentEntry(
+        id: 1,
+        author: 'John Doe',
+        assessmentData: {1: 3, 2: 2, 3: 4},
+      );
+
+      // Compare the actual object with the expected object
+      expect(entry, expectedEntry);
+    });
+  });
+}*/
