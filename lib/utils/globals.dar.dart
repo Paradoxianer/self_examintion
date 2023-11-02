d473f8099820d413d2a3f92357ebe556c4edec7a
paradoxon <blablabla!1865Atlassian>
2023-11-02 17:12:50 +0000

    Added a globals file dart for holing the colors for the questions and the line graph

---
+++ lib/utils/globals.dar.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+const Map<int, Color> globalColorMap = {
+  1: Color(0xFF4285F4), // Blue
+  2: Color(0xFF0F9D58), // Green
+  3: Color(0xFFDB4437), // Red
+  4: Color(0xFFF4B400), // Yellow
+  5: Color(0xFF7CBB00), // Lime
+  6: Color(0xFF673AB7), // Purple
+  7: Color(0xFF00ACC1), // Teal
+  8: Color(0xFFF06292), // Pink
+  9: Color(0xFFFF7043), // Deep Orange
+  10: Color(0xFF9E9E9E), // Grey
+  11: Color(0xFF3F51B5), // Indigo
+  12: Color(0xFFE91E63), // Pink
+  13: Color(0xFFFF5722), // Deep Orange
+  14: Color(0xFFFF9800), // Orange
+  15: Color(0xFF4CAF50), // Green
+  16: Color(0xFF009688), // Teal
+  17: Color(0xFF2196F3), // Blue
+  18: Color(0xFF9C27B0), // Purple
+  19: Color(0xFFFFC107), // Amber
+  20: Color(0xFFFF9800), // Orange
+  21: Color(0xFF795548), // Brown
+  22: Color(0xFF607D8B), // Blue Grey
+  23: Color(0xFF827717), // Lime
+  24: Color(0xFF8D6E63), // Brown
+  25: Color(0xFF455A64), // Blue Grey
+};
