6b4bc7348bfd132aa6c5ee9145eed7951d140189
paradoxon <blablabla!1865Atlassian>
2023-11-02 20:23:36 +0000

    change the chart_screen.dart so that it loads the data from local storage this will enable it to select the Questionset and show the resulst according to the choosen questionset

--- lib/screens/chart_screen.dart
+++ lib/screens/chart_screen.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:self_examintion/models/assessment_entry.dart';
 import 'package:self_examintion/widgets/chart_widget.dart';
+import 'package:self_examintion/utils/local_storage.dart';
 
-class ChartScreen extends Scaffold {
-  final List<AssessmentEntry> assessmentHistory;
-
-  ChartScreen({required this.assessmentHistory})
-      : super(
-    appBar: AppBar(
-      title: Text('Entwicklungsdiagramm'),
-    ),
-    body: ChartWidget(assessmentHistory: assessmentHistory),
-  );
-
+class ChartScreen extends StatefulWidget {
+  @override
+  _ChartScreenState createState() => _ChartScreenState();
 }
 
+class _ChartScreenState extends State<ChartScreen> {
+  List<AssessmentEntry> assessmentHistory = [];
+  final LocalStorage _localStorage = LocalStorage();
+
+  @override
+  void initState() {
+    super.initState();
+    loadAssessmentHistory();
+  }
+
+  Future<void> loadAssessmentHistory() async {
+    try {
+      await _localStorage.initialize();
+      final history = await _localStorage.loadAssessmentEntries();
+      setState(() {
+        assessmentHistory = history;
+      });
+    } catch (e) {
+      // Handle any errors when loading data
+      print('Error loading assessment history: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Entwicklungsdiagramm'),
+      ),
+      body: assessmentHistory.isEmpty
+          ? Center(child: Text('No assessment history available.'))
+          : ChartWidget(assessmentHistory: assessmentHistory),
+    );
+  }
+}
