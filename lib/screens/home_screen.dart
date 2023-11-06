df9a13ca77bdc554f6e4a8d09a881608ef57d094
paradoxon <blablabla!1865Atlassian>
2023-11-06 15:59:06 +0000

    Fixe the german name of this tool

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -16,7 +16,7 @@ class HomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'Willkommen zum Selbstbewertungs-Tool',
+              'Willkommen zum Selbstprüfungs-Tool',
               style: TextStyle(fontSize: 20),
             ),
             SizedBox(height: 20),
