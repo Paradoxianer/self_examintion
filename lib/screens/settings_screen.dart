d2eda1b60ff43537424ce1bfd85d4b9b3d2a4342
paradoxon <blablabla!1865Atlassian>
2023-11-06 17:02:06 +0000

    Enable Selection of the set

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:self_examintion/utils/local_storage.dart';
 import 'package:self_examintion/widgets/notification_settings.dart';
+import 'package:self_examintion/widgets/question_set_selection.dart';
 
 class SettingsScreen extends StatelessWidget {
   final LocalStorage localeStorage = LocalStorage();
@@ -13,15 +14,20 @@ class SettingsScreen extends StatelessWidget {
       ),
       body: ListView(
         children: [
+          QuestionSetSelection(
+            onSetSelected: (selectedSet) {
+              // Handle the selected set (e.g., update settings)
+              print('Selected Set: $selectedSet');
+            },
+          ),
           NotificationsSettings(
             reminderEnabled: false,
             reminderFrequency: 'daily',
-            onChanged: (bool,String){},
+            onChanged: (bool, String) {},
           )
           // Weitere Einstellungen
         ],
       ),
     );
   }
-
 }
