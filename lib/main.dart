8ecf8b45e586470d2e34f3a74ea5a749041fb420
paradoxon <blablabla!1865Atlassian>
2023-12-09 13:28:08 +0100

    added a await ot loadCurrentAutor to make shure we only continue if all the currentAutor is loaded.

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,11 @@ import 'package:self_examintion/localizations/app_localizations.dart';
 import 'package:self_examintion/screens/home_screen.dart';
 import 'package:self_examintion/utils/local_storage.dart';
 
-
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final localStorage = LocalStorage();
   await localStorage.initialize();
-  localStorage.loadCurrentAutor();
+  await localStorage.loadCurrentAutor();
   runApp(MyApp());
 }
 
@@ -19,8 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
-      // Add more MaterialApp configurations as needed.
-      home: HomeScreen()
-    );
+        // Add more MaterialApp configurations as needed.
+        home: HomeScreen());
   }
 }
