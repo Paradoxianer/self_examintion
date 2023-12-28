f303537ca7a3e459ee25f8d0c2d101cb66a9508f
paradoxon <blablabla!1865Atlassian>
2023-12-28 20:33:36 +0100

    Add the posibility to add tips and descriptions to the question model

--- lib/models/question.dart
+++ lib/models/question.dart
@@ -4,14 +4,16 @@ import 'package:crypto/crypto.dart';
 class Question {
   final String id; // Eindeutige ID der Frage, generiert aus dem Text
   final String text; // Der Text der Frage
-  final String? description = null;
+  final String? description; // Beschreibung der Frage
   final bool isPositive;
-  final String? tipps = null;
+  final String? tips; // Tipps um besser zu werden in der entsprechenden Frage
   int answer; // Die vom Benutzer gegebene Antwort (1-4)
 
   Question({
     required this.text,
-    this.isPositive=false,
+    this.description,
+    this.isPositive = false, // Umgekehrte Logik für isPositive
+    this.tips,
     this.answer = 0, // Standardmäßig keine Antwort
   }) : id = generateIdFromText(text); // Automatische Generierung der ID aus dem Text
 
@@ -20,7 +22,9 @@ class Question {
     return {
       'id': id,
       'text': text,
+      'description': description,
       'isPositive': isPositive,
+      'tips': tips,
       'answer': answer,
     };
   }
@@ -29,6 +33,9 @@ class Question {
   factory Question.fromMap(Map<String, dynamic> map) {
     return Question(
       text: map['text'],
+      description: map['description'],
+      isPositive: map['isPositive'],
+      tips: map['tips'],
       answer: map['answer'],
     );
   }
