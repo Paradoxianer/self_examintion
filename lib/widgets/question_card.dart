dd1bdcc1bdeb859c09d6f562c37838c87104801e
paradoxon <blablabla!1865Atlassian>
2024-01-01 23:15:34 +0100

    Add a description as tooltip and a (i) if furthers tipps are handed over... to this object this should fix #6

--- lib/widgets/question_card.dart
+++ lib/widgets/question_card.dart
@@ -19,15 +19,13 @@ class QuestionCard extends StatefulWidget {
 }
 
 class _QuestionCardState extends State<QuestionCard> {
-  double _sliderValue =2.0;
+  double _sliderValue = 2.0;
 
   @override
   void initState() {
     super.initState();
-    _sliderValue = widget.question.answer.toDouble() ;
-    if (_sliderValue == 0)
-      _sliderValue =2.0;
-
+    _sliderValue = widget.question.answer.toDouble();
+    if (_sliderValue == 0) _sliderValue = 2.0;
   }
 
   @override
@@ -35,7 +33,6 @@ class _QuestionCardState extends State<QuestionCard> {
     Color sliderColor = widget.question.isPositive
         ? Color.lerp(Colors.green, Colors.red, _sliderValue / 4) ?? Colors.green
         : Color.lerp(Colors.red, Colors.green, _sliderValue / 4) ?? Colors.red;
-
 
     return Card(
       margin: EdgeInsets.all(8.0),
@@ -57,29 +54,117 @@ class _QuestionCardState extends State<QuestionCard> {
               ),
             ),
             Expanded(
-              child: Text(
-                widget.question.text,
-                style: TextStyle(fontSize: 16.0),
+              child: Tooltip(
+                message: widget.question.description ?? '',
+                child: Text(
+                  widget.question.text,
+                  style: TextStyle(fontSize: 16.0),
+                ),
               ),
             ),
-            Slider(
-                value: _sliderValue,
-                onChanged: (newValue) {
-                  setState(() {
-                    _sliderValue = newValue;
-                    widget.onSliderChanged(newValue); // Call the callback to update slider value
-                  });
-                },
-              min: 1,
-              max: 4,
-              divisions: 3,
-              activeColor: sliderColor,
-              inactiveColor: sliderColor.withOpacity(0.5),
-              label: AppLocalizations.of(context)!.answers[_sliderValue.toInt()-1]
+            widget.question.tips != null ? IconButton(
+              icon: Icon(Icons.info),
+              onPressed: () {
+                _showTipsDialog(context, widget.question.tips ?? '');
+              },
+            ) : Container(),
+            Row(
+              children: [
+         //       Text(AppLocalizations.of(context)!.answers[0]),
+                SliderTheme(
+                  child: Slider(
+                      value: _sliderValue,
+                      onChanged: (newValue) {
+                        setState(() {
+                          _sliderValue = newValue;
+                          widget.onSliderChanged(newValue); // Call the callback to update slider value
+                        });
+                      },
+                      min: 1,
+                      max: 4,
+                      divisions: 3,
+                      activeColor: sliderColor,
+                      inactiveColor: sliderColor,
+                      label: AppLocalizations.of(context)!.answers[_sliderValue.toInt() - 1],
+                    ),
+                  data: SliderTheme.of(context).copyWith(
+                    trackHeight: 5.0
+                  ),
+                ),
+           //     Text(AppLocalizations.of(context)!.answers.last,softWrap: true,)
+              ],
             ),
           ],
         ),
       ),
     );
+  }
+
+  void _showTipsDialog(BuildContext context, String tips) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Tipps'),
+          content: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: _buildTipWidgets(context, tips),
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Schließen'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  List<Widget> _buildTipWidgets(BuildContext context, String tips) {
+    List<Widget> tipWidgets = [];
+
+    // Split the tips string into lines
+    List<String> lines = tips.split('\n');
+
+    for (String line in lines) {
+      if (line.isNotEmpty) {
+        // Check if the line contains a clickable link
+        final linkMatch = RegExp(r'\[(.+?)\]\((\S+?)\)').firstMatch(line);
+        if (linkMatch != null) {
+          String linkText = linkMatch.group(1)!;
+          String linkUrl = linkMatch.group(2)!;
+
+          tipWidgets.add(
+            InkWell(
+              onTap: () {
+                // Open the link when tapped
+                // Note: You can implement your own logic to open the link
+                // e.g., launch(linkUrl)
+              },
+              child: Text(
+                linkText,
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          );
+        } else {
+          // Regular text without a clickable link
+          tipWidgets.add(Text(line));
+        }
+
+        // Add a padding between lines
+        tipWidgets.add(SizedBox(height: 8));
+      }
+    }
+
+    return tipWidgets;
   }
 }
